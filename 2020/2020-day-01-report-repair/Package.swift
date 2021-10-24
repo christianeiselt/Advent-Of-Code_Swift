98affb317fa3ee6d90249cd45619c491416cf90d
Christian Eiselt <dev@eiselt.ch>
2021-10-24 22:08:22 +0200

    Initial Setup

--- 2020/2020-day-01-report-repair/Package.swift
+++ 2020/2020-day-01-report-repair/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,20 +12,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "Main",
-            dependencies: ["Puzzle"],
-            resources: [
-                .process("input.txt")]
-        ),
+            dependencies: ["Puzzle"]),
         .target(
             name: "Puzzle",
             dependencies: ["AdventKit"]),
         .testTarget(
             name: "PuzzleTests",
-            dependencies: ["Puzzle"],
-            resources: [
-                .process("example.txt")]
-        ),
+            dependencies: ["Puzzle"]),
     ]
 )
--- 2020/2020-day-01-report-repair/Tests/PuzzleTests/PuzzleTests.swift
+++ 2020/2020-day-01-report-repair/Tests/PuzzleTests/PuzzleTests.swift
@@ -5,8 +5,8 @@ final class PuzzleTests: XCTestCase {
     func testExample() throws {
         
         let puzzle = Puzzle(input: "")
-        XCTAssertEqual(puzzle.part1(), 514_579)
-        XCTAssertEqual(puzzle.part2(), 241_861_950)
+        XCTAssertEqual(puzzle.part1(), 0)
+        XCTAssertEqual(puzzle.part2(), 0)
     }
 
     static var allTests = [
