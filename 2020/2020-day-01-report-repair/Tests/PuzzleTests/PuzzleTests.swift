6d09e962d663bc964e7804425212e3201c81c7d6
Christian Eiselt <dev@eiselt.ch>
2021-10-25 23:27:28 +0200

    Try #1 to use input files

--- 2020/2020-day-01-report-repair/Tests/PuzzleTests/PuzzleTests.swift
+++ 2020/2020-day-01-report-repair/Tests/PuzzleTests/PuzzleTests.swift
@@ -1,9 +1,10 @@
 @testable import Puzzle
+import AdventKit
 import XCTest
 
 final class PuzzleTests: XCTestCase {
     func testExample() throws {
-        
+
         let puzzle = Puzzle(input: "")
         XCTAssertEqual(puzzle.part1(), 0)
         XCTAssertEqual(puzzle.part2(), 0)
