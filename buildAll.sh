ba98166d3487f67ff6705ed7dfc40b82a2c547f4
Christian Eiselt <dev@eiselt.ch>
2021-10-26 23:08:06 +0200

    update scripts for ubuntu compatibility

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find -s . -name "*day*" -depth 2 -type d \
+find . -depth -mindepth 2 -maxdepth 2 -name "*day*" -type d \
     -exec echo Building {} \; \
     -exec sh -c 'pushd . > /dev/null; cd $1; swift build -c release; popd > /dev/null' sh {} \;
--- runAll.sh
+++ runAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find -s . -name "*day*" -depth 2 -type d \
+find . -depth -mindepth 2 -maxdepth 2 -name "*day*" -type d \
     -exec echo Running {} \; \
     -exec sh -c 'pushd .  > /dev/null; cd $1; swift run -c release; popd > /dev/null' sh {} \;
