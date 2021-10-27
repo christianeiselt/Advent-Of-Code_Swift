713e73051f6c2330204c589159926850b2e12ca1
Christian Eiselt <dev@eiselt.ch>
2021-10-27 22:36:35 +0200

    Add start to 2015

---
+++ 2015/2015-day-01-not-quite-lisp/Tests/PuzzleTests/PuzzleTests.swift
@@ -0,0 +1,17 @@
+@testable import Puzzle
+import AdventKit
+import XCTest
+
+final class PuzzleTests: XCTestCase {
+    func testExample() throws {
+
+        let puzzle = Puzzle(input: exampleText)
+
+        XCTAssertEqual(puzzle.part1(), 0)
+        XCTAssertEqual(puzzle.part2(), 0)
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
