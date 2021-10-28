2a8d7c19cc792f13494295ada69b5ca65f8b5c89
Christian Eiselt <dev@eiselt.ch>
2021-10-28 21:28:27 +0200

    update scripts

--- buildAll.sh
+++ buildAll.sh
@@ -2,4 +2,4 @@
 
 find . -depth -mindepth 2 -maxdepth 2 -name "*day*" -type d \
     -exec echo Building {} \; \
-    -exec sh -c 'pushd . > /dev/null; cd $1; swift build -c release; popd > /dev/null' sh {} \;
+    -exec sh -c 'pushd . > /dev/null; cd "$1"; swift build -c release; popd > /dev/null' sh {} \;
--- testAll.sh
+++ testAll.sh
@@ -2,4 +2,4 @@
 
 find . -depth -mindepth 2 -maxdepth 2 -name "*day*" -type d \
     -exec echo Running {} \; \
-    -exec sh -c 'pushd .  > /dev/null; cd $1; swift test -v; popd > /dev/null' sh {} \;
+    -exec sh -c 'pushd .  > /dev/null; cd "$1"; swift test -v; popd > /dev/null' sh {} \;
