0182809b26b530c256713c36ce9604bb73f711f9
Christian Eiselt <dev@eiselt.ch>
2021-10-28 22:12:55 +0200

    CDE-362 Finish 2015Day01

--- 2015/2015-day-01-not-quite-lisp/Sources/Puzzle/Puzzle.swift
+++ 2015/2015-day-01-not-quite-lisp/Sources/Puzzle/Puzzle.swift
@@ -3,11 +3,21 @@ import Foundation
 
 public class Puzzle {
     let input: String
-    public init(input: String) {
-        self.input = input
-    }
+    let instructions: [Character]
     let moveUp: Character = "("
     let moveDown: Character = ")"
+
+    enum Part {
+        case One
+        case Two
+    }
+
+    public init(input: String) {
+        self.input = input
+        self.instructions = input.map { (character) -> Character in
+            return character
+        }
+    }
 
     private func move(instruction: Character) -> Int {
         if instruction == moveUp {
@@ -19,35 +29,26 @@ public class Puzzle {
         }
     }
 
-    public func part1() -> Int {
-        let instructions = input.map { (character) -> Character in
-              return character
-        }
-
-        var floor = 0
-
-        for index in 0..<instructions.count {
-            floor += move(instruction: instructions[index])
-        }
-
-        return floor
-    }
-
-    public func part2() -> Int {
-        let instructions = input.map { (character) -> Character in
-              return character
-        }
+    private func followInstructions(instructions: [Character], part: Part) -> Int {
 
         var floor = 0
 
         for index in 0..<instructions.count {
             floor += move(instruction: instructions[index])
 
-            if floor == -1 {
+            if part == Part.Two && floor == -1 {
                 return index + 1
             }
         }
 
         return floor
+    }
+
+    public func part1() -> Int {
+        return followInstructions(instructions: instructions, part: Part.One)
+    }
+
+    public func part2() -> Int {
+        return followInstructions(instructions: instructions, part: Part.Two)
     }
 }
