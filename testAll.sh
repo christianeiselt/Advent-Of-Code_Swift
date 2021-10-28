f7ef0ec8e71de2a56aa5c9ab808023d6e019023a
Christian Eiselt <dev@eiselt.ch>
2021-10-28 21:45:52 +0200

    update scripts

--- testAll.sh
+++ testAll.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find . -depth -mindepth 2 -maxdepth 2 -name "*day*" -type d \
+find -s . -name "*day*" -depth 1 -type d \
     -exec echo Running {} \; \
-    -exec sh -c 'pushd .  > /dev/null; cd "$1"; swift test -v; popd > /dev/null' sh {} \;
+    -exec sh -c 'pushd .  > /dev/null; cd $1; swift test; popd > /dev/null' sh {} \;
