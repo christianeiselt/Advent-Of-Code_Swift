34fc89ea65fce60159c5c3ad37e0b827fb574750
Christian Eiselt <dev@eiselt.ch>
2021-10-28 22:15:54 +0200

    disable shellcheck SC2016

--- buildAll.sh
+++ buildAll.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# We want this to output $PATH without expansion
+# shellcheck disable=SC2016
 find -s . -name "*day*" -depth 2 -type d \
     -exec echo Building {} \; \
     -exec sh -c 'pushd . > /dev/null; cd "$1"; swift build -c release; popd > /dev/null' sh {} \;
--- runAll.sh
+++ runAll.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# We want this to output $PATH without expansion
+# shellcheck disable=SC2016
 find -s . -name "*day*" -depth 2 -type d \
     -exec echo Running {} \; \
     -exec sh -c 'pushd .  > /dev/null; cd "$1"; swift run -c release; popd > /dev/null' sh {} \;
