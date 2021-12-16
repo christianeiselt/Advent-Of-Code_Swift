9c271698e9c8c4d909eadd439e0103bf32db0b40
Christian Eiselt <dev@eiselt.ch>
2021-12-16 21:54:21 +0100

    fix bash issues

--- runAll.sh
+++ runAll.sh
@@ -3,5 +3,5 @@
 # We want this to output $PATH without expansion
 # shellcheck disable=SC2016
 find -s . -name "*day*" -depth 2 -type d \
-    -exec echo Running {} \; \
-    -exec sh -c 'pushd .  > /dev/null; cd "$1"; swift run -c release; popd > /dev/null' sh {} \;
+	-exec echo Running {} \; \
+	-exec sh -c 'pushd .  > /dev/null; cd "$1"; swift run -c release; popd > /dev/null' sh {} \;
