0978ad81857ff4e79bb7a7bc1f6c541738bd2a2b
Christian Eiselt <dev@eiselt.ch>
2021-12-28 15:23:50 +0100

    fix typo

--- buildAll.sh
+++ buildAll.sh
@@ -7,7 +7,7 @@ while IFS= read -r -d $'\0'; do
 done < <(find . -name "*day*" -type d -prune -print0 | sort -z)
 
 for package in "${packages[@]}"; do
-	echo "Running $package"
+	echo "Building $package"
 	cd "$package" || exit
 	swift build -c release
 	cd - || exit
