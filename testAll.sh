47498b219e6272242917ab40dd3455006cc99eae
Christian Eiselt <dev@eiselt.ch>
2021-12-28 15:23:44 +0100

    fix typo; add cov conversion

--- testAll.sh
+++ testAll.sh
@@ -7,8 +7,9 @@ while IFS= read -r -d $'\0'; do
 done < <(find . -name "*day*" -type d -prune -print0 | sort -z)
 
 for package in "${packages[@]}"; do
-	echo "Running $package"
+	echo "Testing $package"
 	cd "$package" || exit
-	swift test -v --enable-code-coverage --show-codecov-path
+	swift test --enable-test-discovery --enable-code-coverage
+	llvm-cov export -format="lcov" .build/debug/*.xctest -instr-profile .build/debug/codecov/default.profdata >info.lcov
 	cd - || exit
 done
