d09137bacb7988805b9f1d380a9a1b2bcef607cd
Christian Eiselt <dev@eiselt.ch>
2022-01-05 23:51:13 +0100

    puzzle(2015d2): add test cases for part two

--- 2015/2015-day-02-i-was-told-there-would-be-no-math/Tests/PuzzleTests/ExampleInput.swift
+++ 2015/2015-day-02-i-was-told-there-would-be-no-math/Tests/PuzzleTests/ExampleInput.swift
@@ -9,3 +9,5 @@ import Foundation
 
 let example1 = (input: "2x3x4", result: 58)
 let example2 = (input: "1x1x10", result: 43)
+let example3 = (input: "2x3x4", result: 34)
+let example4 = (input: "1x1x10", result: 14)
